d0e68b72b6bb470fd2db0ef04295ce8370b37fd8
Mateo Tibaquira <nestormateo@gmail.com>
2021-04-01 23:34:26 -0500

    initial commit

---
+++ apps/demos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'open-source-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.styl'],
+})
+export class AppComponent {
+  title = 'demos';
+}
