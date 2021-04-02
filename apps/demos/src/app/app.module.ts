e5c19ac8b3c5183ebf690f56f36d874beaeefba1
Mateo Tibaquira <nestormateo@gmail.com>
2021-04-02 01:40:01 -0500

    build: ng add @angular/material

--- apps/demos/src/app/app.module.ts
+++ apps/demos/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
+  imports: [BrowserAnimationsModule],
   declarations: [AppComponent],
-  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
