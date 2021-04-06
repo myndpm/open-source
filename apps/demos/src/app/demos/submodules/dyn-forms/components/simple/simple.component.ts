d40f2e440bbd86fdc98e1593aaf4b58bb98b8b74
Mateo Tibaquira <nestormateo@gmail.com>
2021-04-06 17:44:41 -0500

    refactor(demos): extract the dyn-form links from the component

--- apps/demos/src/app/demos/submodules/dyn-forms/components/simple/simple.component.ts
+++ apps/demos/src/app/demos/submodules/dyn-forms/components/simple/simple.component.ts
@@ -7,9 +7,9 @@ import {
 } from '@angular/core';
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { DynFormComponent } from '@myndpm/dyn-forms';
-import { SectionAction, SectionBadge } from 'apps/demos/src/app/layout';
 import { BehaviorSubject } from 'rxjs';
 import { startWith } from 'rxjs/operators';
+import { actions, badges } from '../../constants/dyn-forms.links';
 import { simpleData, simpleForm } from './simple.form';
 
 @Component({
@@ -21,35 +21,8 @@ import { simpleData, simpleForm } from './simple.form';
 })
 export class SimpleComponent implements AfterViewInit {
   // ref links
-  badges: SectionBadge[] = [
-    {
-      link: "https://github.com/Mynd-Management/open-source/tree/master/libs/forms",
-      img: "https://img.shields.io/badge/%40myndpm-dyn--forms-brightgreen",
-      alt: "Package",
-    },
-    {
-      link: "https://www.npmjs.com/package/@myndpm/dyn-forms",
-      img: "https://badge.fury.io/js/%40myndpm%2Fdyn-forms.svg",
-      alt: "NPM Badge",
-    },
-    {
-      link: "https://npmcharts.com/compare/@myndpm/dyn-forms?minimal=true",
-      img: "https://img.shields.io/npm/dm/@myndpm/dyn-forms.svg?style=flat",
-      alt: "NPM Downloads",
-    },
-  ];
-  actions: SectionAction[] = [
-    {
-      link: "https://github.com/Mynd-Management/open-source/blob/master/apps/demos/src/app/demos/submodules/dyn-forms/components/simple/simple.form.ts",
-      icon: 'code',
-      tooltip: 'See source code',
-    },
-    {
-      link: "https://prezi.com/view/4Ok1bgCWvf0g26FMVwfx/",
-      icon: 'preview',
-      tooltip: 'Prezi',
-    },
-  ];
+  badges = badges;
+  actions = actions;
 
   // reactive parameters for the billing CARD
   profileCard = new BehaviorSubject({
---
+++ apps/demos/src/app/demos/submodules/dyn-forms/constants/dyn-forms.links.ts
@@ -0,0 +1,32 @@
+import { SectionAction, SectionBadge } from 'apps/demos/src/app/layout';
+
+export const badges: SectionBadge[] = [
+  {
+    link: "https://github.com/Mynd-Management/open-source/tree/master/libs/forms",
+    img: "https://img.shields.io/badge/%40myndpm-dyn--forms-brightgreen",
+    alt: "Package",
+  },
+  {
+    link: "https://www.npmjs.com/package/@myndpm/dyn-forms",
+    img: "https://badge.fury.io/js/%40myndpm%2Fdyn-forms.svg",
+    alt: "NPM Badge",
+  },
+  {
+    link: "https://npmcharts.com/compare/@myndpm/dyn-forms?minimal=true",
+    img: "https://img.shields.io/npm/dm/@myndpm/dyn-forms.svg?style=flat",
+    alt: "NPM Downloads",
+  },
+];
+
+export const actions: SectionAction[] = [
+  {
+    link: "https://github.com/Mynd-Management/open-source/blob/master/apps/demos/src/app/demos/submodules/dyn-forms/components/simple/simple.form.ts",
+    icon: 'code',
+    tooltip: 'See source code',
+  },
+  {
+    link: "https://prezi.com/view/4Ok1bgCWvf0g26FMVwfx/",
+    icon: 'preview',
+    tooltip: 'Prezi',
+  },
+];
