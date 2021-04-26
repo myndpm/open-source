3d9475c42948e0a81fcea856b4b6a8dd7635cf9e
Mateo Tibaquira <nestormateo@gmail.com>
2021-04-25 21:20:58 -0500

    feat(website): queryParam for active tab

--- apps/website/src/app/docs/components/content/content.component.ts
+++ apps/website/src/app/docs/components/content/content.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { MatTabChangeEvent } from '@angular/material/tabs';
-import { ActivatedRoute } from '@angular/router';
+import { ChangeDetectionStrategy, Component, DoCheck, Input, OnChanges, ViewChild } from '@angular/core';
+import { MatTabChangeEvent, MatTabGroup } from '@angular/material/tabs';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { DocsLocalized, DocsMetadata } from '../../interfaces';
 import { ContentService, I18nService } from '../../services';
@@ -11,8 +11,10 @@ import { ContentService, I18nService } from '../../services';
   styleUrls: ['./content.component.styl'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ContentComponent {
+export class ContentComponent implements DoCheck, OnChanges {
   @Input() metadata: DocsMetadata;
+
+  @ViewChild(MatTabGroup) tabs: MatTabGroup;
 
   source = new Subject<string>();
 
@@ -24,9 +26,24 @@ export class ContentComponent {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private content: ContentService,
     private i18n: I18nService,
   ) {}
+
+  ngDoCheck(): void {
+    if (this.tabs) {
+      const active = this.route.snapshot.queryParamMap.get('active');
+      if (active) {
+        const tab = this.tabs._allTabs.find(
+          tab => tab.textLabel.toLowerCase() === active
+        );
+        if (tab.position) {
+          this.tabs.selectedIndex = tab.position;
+        }
+      }
+    }
+  }
 
   ngOnChanges(): void {
     if (this.metadata?.content) {
@@ -38,7 +55,17 @@ export class ContentComponent {
     }
   }
 
-  onTabChange({ tab }: MatTabChangeEvent): void {
+  onTabChange({ index, tab }: MatTabChangeEvent): void {
+    // update the URL
+    this.router.navigate(
+      [],
+      {
+        relativeTo: this.route,
+        queryParams: index !== 0
+          ? { active: tab.textLabel.toLowerCase() } // FIXME update i18n
+          : {},
+      });
+
     if (tab.textLabel === 'Examples') { // FIXME update i18n
       this.loadExamples = true;
     }
