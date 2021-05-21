81248c02684bc82e4c3d24f8a0bcf99459abe7e4
Mateo Tibaquira <nestormateo@gmail.com>
2021-05-20 22:45:26 -0500

    fix(dyn-forms): log in console when a control is missing

--- libs/forms/core/src/form-registry.service.ts
+++ libs/forms/core/src/form-registry.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
+import { DynLogger } from '@myndpm/dyn-forms/logger';
 import { DynControlProvider } from './control-provider.types';
 import { DynControlType, DynInstanceType } from './control.types';
 import { DYN_CONTROLS_TOKEN } from './form.tokens';
@@ -6,14 +7,17 @@ import { DYN_CONTROLS_TOKEN } from './form.tokens';
 @Injectable()
 export class DynFormRegistry {
   constructor(
-    @Inject(DYN_CONTROLS_TOKEN) private readonly controls: DynControlProvider[]
+    private readonly logger: DynLogger,
+    @Inject(DYN_CONTROLS_TOKEN) private readonly controls: DynControlProvider[],
   ) {}
 
   get(dynControl: DynControlType): DynControlProvider {
     const provided = this.controls.find(({ control }) => dynControl === control);
 
     if (!provided) {
-      throw new Error(`Control '${dynControl}' not provided!`);
+      const error = this.logger.providerNotFound('Control', dynControl);
+      console.error(error.message);
+      throw error;
     }
 
     return provided;
