8ed80e751ebd4b83879547d2c831abd6965cf0d6
Mateo Tibaquira <nestormateo@gmail.com>
2022-03-11 00:18:13 -0500

    feat(stylus2scss): add program version from package.json

--- libs/tools/stylus2scss/src/index.ts
+++ libs/tools/stylus2scss/src/index.ts
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import { readFileSync } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 import { parseOptions } from './schema.js';
+
+const pkgjson = join(dirname(fileURLToPath(import.meta.url)), '../package.json');
+const metadata = JSON.parse(readFileSync(pkgjson).toString());
 
 const program = new Command();
 
 program
   .name('stylus2scss')
+  .version(metadata.version)
   .description('CLI util to convert Stylus to SCSS');
 
 program
