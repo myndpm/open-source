c0da04b390b92937fa0d785471725f9e33e1b77a
Mateo Tibaquira <nestormateo@gmail.com>
2022-03-11 22:58:04 -0500

    feat(stylus2scss): add --diagnose output

--- libs/tools/stylus2scss/src/index.ts
+++ libs/tools/stylus2scss/src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { FsTree } from '@myndpm/utils';
+import chalk from 'chalk';
 import { Command } from 'commander';
 import { readFileSync } from 'fs';
 import { dirname, join, relative } from 'path';
@@ -19,7 +20,7 @@ program
 
 program
   .option('-p, --path <path>', 'Path to convert. Defaults to current', '.')
-  .option('--diagnose', 'Diagnose the stylesheets in the folder', false)
+  .option('--diagnose', 'List the files to process in the folder', false)
   .option('--only-migrate', 'Only run sass-migration on SCSS files', false);
 
 program.parse(process.argv);
@@ -27,9 +28,34 @@ program.parse(process.argv);
 const options = parseOptions(program.opts());
 const fstree = new FsTree();
 
+console.log(chalk.cyanBright(`stylus2scss ${
+  options.diagnose ? 'diagnosis' : (options.onlyMigrate ? 'migration' : 'conversion')
+}`));
+
 fstree.visit(options.path, (path) => {
-  console.log(relative(options.path, path))
+  if (!(path.endsWith('.styl') || options.onlyMigrate && path.endsWith('.scss'))) {
+    return;
+  }
+
+  let filepath = path;
+
   // diagnose
+  if (options.diagnose && !options.onlyMigrate) {
+    console.log(chalk.dim('>', relative(options.path, path)));
+  }
+
   // convert
+  if (!options.diagnose && !options.onlyMigrate) {
+    filepath = filepath.replace(/\.styl$/, '.scss');
+    // TODO perform conversion
+  }
+
   // migrate
+  if (filepath.endsWith('.scss')) {
+    if (options.diagnose) {
+      console.log(chalk.dim('>', relative(options.path, filepath)));
+    } else {
+      // TODO perform migration
+    }
+  }
 })
