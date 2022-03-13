d00bdc58f21880dfb56e4a83e450523b5a66a46b
Mateo Tibaquira <nestormateo@gmail.com>
2022-03-12 20:09:32 -0500

    feat(utils): add rxjs fs utils

--- libs/tools/utils/src/index.ts
+++ libs/tools/utils/src/index.ts
@@ -1,3 +1,4 @@
 export * from './fs-tree';
 export * from './json';
+export * from './rxjs-fs';
 export * from './rxjs-shell';
---
+++ libs/tools/utils/src/rxjs-fs.ts
@@ -0,0 +1,8 @@
+export {
+  appendFile,
+  readDir,
+  readFile,
+  stat,
+  watch,
+  writeFile,
+} from '@ckapp/rxjs-node-fs';
