0fa0785a44e4bda14b8c79c1c1ddbd9be46f874e
Mateo Tibaquira <nestormateo@gmail.com>
2022-08-08 20:16:58 -0500

    refactor(forms): DynWrapper extends DynControl

--- libs/forms/core/src/dyn-control-wrapper.class.ts
+++ libs/forms/core/src/dyn-control-wrapper.class.ts
@@ -1,9 +1,8 @@
 import { Directive, OnDestroy, OnInit, Type, ViewChild, ViewContainerRef } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 import { DynParams } from './types/params.types';
 import { DynBaseProvider } from './types/provider.types';
 import { DynWrapperConfig, DynWrapperId } from './types/wrapper.types';
-import { DynControlNode } from './dyn-control-node.class';
+import { DynControl } from './dyn-control.class';
 
 export type AbstractDynWrapper = DynWrapper;
 
@@ -17,22 +16,13 @@ export abstract class DynWrapper<
   TParams extends DynParams = DynParams,
   TConfig extends DynWrapperConfig<TParams> = DynWrapperConfig<TParams>,
 >
-extends DynControlNode<TParams, any>
+extends DynControl<string, TParams, any, any>
 implements OnInit, OnDestroy {
   @ViewChild('dynContainer', { read: ViewContainerRef, static: true })
   container!: ViewContainerRef;
 
   config!: TConfig;
-  get params(): TParams {
-    return this.params$.getValue();
-  }
-
-  readonly params$ = new BehaviorSubject<TParams>({} as TParams);
 
   ngOnInit(): void {
-  }
-
-  ngOnDestroy(): void {
-    this.params$.complete();
   }
 }
