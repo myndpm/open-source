28e6d8406088ef83b6bdd8aff067ee25556cfd6a
Mateo Tibaquira <nestormateo@gmail.com>
2022-09-02 11:47:38 -0500

    fix(pug2html): show warnings for known issues

--- libs/tools/pug2html/src/converter/pug2html.ts
+++ libs/tools/pug2html/src/converter/pug2html.ts
@@ -1,8 +1,9 @@
 import { readFile, writeFile } from '@myndpm/utils';
+import { relative } from 'path';
 import { Observable, of } from 'rxjs';
 import { concatMap } from 'rxjs/operators';
 import { Schema } from '../schema';
-import { logInfo } from '../utils';
+import { logInfo, logNote } from '../utils';
 import { converter } from './converter';
 
 export function pugConvert(opts: Required<Schema>): Observable<any> {
@@ -12,7 +13,16 @@ export function pugConvert(opts: Required<Schema>): Observable<any> {
   }
   return readFile(opts.file).pipe(
     concatMap((content) => {
-      return writeFile(opts.file, converter(content));
+      const file = relative(opts.path, opts.file);
+      let source = converter(content);
+      // show wanings
+      if (source.indexOf('$implicit') !== -1) {
+        logNote(`please check how let-* of $implicit were converted: ${file}`);
+      }
+      if (source.indexOf('ngForOf') !== -1) {
+        logNote(`please check how let-* of ngForOf were converted: ${file}`);
+      }
+      return writeFile(opts.file, source);
     }),
   );
 }
