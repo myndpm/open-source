19e33fc6e3a6ba79eab6f488e8ef5f3d66569731
Mateo Tibaquira <nestormateo@gmail.com>
2022-09-16 21:39:19 -0500

    fix: release @myndpm/dyn-forms@12.5.1
    Extend isDynHidden util to check DynControlConfig

--- libs/forms/core/src/utils/hidden.util.ts
+++ libs/forms/core/src/utils/hidden.util.ts
@@ -1,3 +1,8 @@
-export function isDynHidden(control: string): boolean {
-  return control === 'HIDDEN' || control === 'HIDDEN-GROUP';
+import { DynControlConfig } from '../types/control.types';
+
+export function isDynHidden(control: string | DynControlConfig): boolean {
+  const id: string = typeof control !== 'string'
+    ? control.control
+    : control;
+  return id === 'HIDDEN' || id === 'HIDDEN-GROUP';
 }
